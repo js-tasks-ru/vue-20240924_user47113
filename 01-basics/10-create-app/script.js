bde6e7015bfc5c26a303f4670f08588cc4f04b48
Dmitriy Andreev <i@vajrock.ru>
2024-09-30 10:49:12 +0300

    01-basics добавлен компонент для вывода даты

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,22 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'app',
+
+  setup() {
+    function printDateAsLocalDate() {
+      return new Date().toLocaleString(navigator.language, { dateStyle: 'long' })
+    }
+    return {
+      printDateAsLocalDate,
+    }
+  },
+
+  template: `
+    <div>Сегодня {{printDateAsLocalDate()}}</div>
+  `,
+})
+
+const app = createApp(App)
+
+const vm = app.mount('#app')
